1895df19e4724c850157a04acb00a71c39fef9d4
Mylo Yu <emdoubleu.yu@gmail.com>
2025-06-17 19:17:50 -0700

    Added a part scene with logic to drag and drop around

---
+++ Scenes/part.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+var hoveringOver := false
+var grabbed := false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Interact") && hoveringOver:
+		grabbed = true
+	if Input.is_action_just_released("Interact") && grabbed:
+		grabbed = false
+	if grabbed:
+		position = get_viewport().get_mouse_position()
+		if Input.is_action_just_pressed("Rotate"):
+			rotation += PI/2
+		
+
+func _on_mouse_entered() -> void:
+	hoveringOver = true
+
+func _on_mouse_exited() -> void:
+	hoveringOver = false
